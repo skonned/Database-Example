b693ce66a91c878ccee6028ea21919ce6a69237f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-10 10:51:41 +1200

    tested database again

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+#docstring- Darren Leung- animal database application
+#imports
 import sqlite3
 
-db = sqlite3.connect("animals.db")
-cursor = db.cursor()
-sql = "SELECT * FROM animal;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+#constants and variables
+DATABASE = "animals.db"
+
+
+#functions
+def print_all_animals():
+    '''print all the animals nicely'''
+    db = sqlite3.connect("animals.db")
+    cursor = db.cursor()
+    sql = "SELECT * FROM animal;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Name                Species                       Habitat                                 Diet                                    Average_lifespan              Average_weight                Status")
+    for animal in results:
+        print(f"{animal[1]:<20}{animal[2]:<30}{animal[3]:<40}{animal[4]:<40}{animal[5]:<30}{animal[6]:<30}{animal[7]:<30}")
+    #loop finished here
+    db.close()
+
+
+
+
+#main code
+print_all_animals()
